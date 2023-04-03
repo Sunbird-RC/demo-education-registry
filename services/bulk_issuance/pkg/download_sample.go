422841d83772e0ab7a6f81f1c84342c606d78872
Tejash <tejash.jl1020@gmail.com>
2023-04-03 16:27:36 +0530

    update bulk sample api to return sample value for the properties

--- services/bulk_issuance/pkg/download_sample.go
+++ services/bulk_issuance/pkg/download_sample.go
@@ -14,16 +14,17 @@ import (
 func downloadSampleFile(params sample_template.GetV1BulkSampleSchemaNameParams, principal *models.JWTClaimBody) middleware.Responder {
 	log.Info("Downloading sample file")
 	response := sample_template.GetV1BulkSampleSchemaNameOK{}
-	schemaProperties, err := utils.GetSchemaProperties(params.SchemaName)
+	schemaProperties, sampleValues, err := utils.GetSchemaPropertiesAndSampleValues(params.SchemaName)
 	if err != nil {
 		response := sample_template.NewGetV1BulkSampleSchemaNameNotFound()
 		response.SetPayload(err.Error())
 		return response
 	}
-	headers := make([][]string, 0)
-	headers = append(headers, schemaProperties)
-	log.Infof("Headers for schema %v : %v", params.SchemaName, headers)
-	file_service.CreateFile(params.SchemaName+".csv", headers)
+	csvData := make([][]string, 0)
+	csvData = append(csvData, schemaProperties)
+	csvData = append(csvData, sampleValues)
+	log.Infof("Headers for schema %v : %v", params.SchemaName, csvData)
+	file_service.CreateFile(params.SchemaName+".csv", csvData)
 	f, err := os.Open(params.SchemaName + ".csv")
 	utils.LogErrorIfAny("Error while opening file : %v", err)
 	response.WithContentDisposition("attachment; filename=\"" + params.SchemaName + ".csv\"").WithPayload(f)
--- services/bulk_issuance/utils/utils.go
+++ services/bulk_issuance/utils/utils.go
@@ -4,9 +4,11 @@ import (
 	"bulk_issuance/config"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
+	"github.com/lucasjones/reggen"
+	"io"
 	"net/http"
 	"sort"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -21,9 +23,38 @@ func Contains(arr []string, str string) bool {
 }
 
 func GetSchemaProperties(key string) ([]string, error) {
+	schemaProperties, err := getJsonSchemaProperties(key)
+	if err == nil {
+		properties := make([]string, 0)
+		for k := range schemaProperties {
+			properties = append(properties, k)
+		}
+		sort.Strings(properties)
+		return properties, nil
+	} else {
+		return nil, err
+	}
+}
+
+func GetSchemaPropertiesAndSampleValues(key string) ([]string, []string, error) {
+	schemaProperties, err := getJsonSchemaProperties(key)
+	if err == nil {
+		properties := make([]string, 0)
+		sampleValues := make([]string, 0)
+		for key, value := range schemaProperties {
+			properties = append(properties, key)
+			sampleValues = append(sampleValues, getSampleValueByType(value.(map[string]interface{})))
+		}
+		sort.Strings(properties)
+		return properties, sampleValues, nil
+	}
+	return nil, nil, err
+}
+
+func getJsonSchemaProperties(key string) (map[string]interface{}, error) {
 	resp, err := http.Get(config.Config.Registry.BaseUrl + "api/docs/swagger.json")
 	LogErrorIfAny("Error creating a get request for %v : %v", err, config.Config.Registry.BaseUrl+"api/docs/swagger.json")
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 	var responseMap map[string]interface{}
 	err = json.Unmarshal(body, &responseMap)
 	LogErrorIfAny("Error creating request body for %v : %v", err, config.Config.Registry.BaseUrl+"api/docs/swagger.json")
@@ -32,12 +63,52 @@ func GetSchemaProperties(key string) ([]string, error) {
 		return nil, errors.New(key + " not found")
 	}
 	schemaProperties := schemaMap.(map[string]interface{})["properties"].(map[string]interface{})
-	properties := make([]string, 0)
-	for k := range schemaProperties {
-		properties = append(properties, k)
+	return schemaProperties, nil
+}
+
+func getSampleValueByType(value map[string]interface{}) string {
+	if jsonType, ok := value["type"]; ok {
+		switch jsonType {
+		case "string":
+			if format, ok := value["format"]; ok {
+				return getSampleStringValueBasedOnFormat(format.(string))
+			}
+			if pattern, ok := value["pattern"]; ok {
+				if randomString, err := generateRandomString(pattern); err == nil {
+					return randomString
+				}
+			}
+		case "number":
+			return "0"
+		case "integer":
+			return "0"
+		case "object":
+			return "{}"
+		case "array":
+			return "[]"
+		case "boolean":
+			return "true"
+		}
 	}
-	sort.Strings(properties)
-	return properties, nil
+	return "string"
+}
+
+func generateRandomString(pattern interface{}) (string, error) {
+	return reggen.Generate(pattern.(string), 1)
+}
+
+func getSampleStringValueBasedOnFormat(format string) string {
+	switch format {
+	case "date":
+		currentTime := time.Now()
+		return currentTime.Format(time.DateOnly)
+	case "date-time":
+		currentTime := time.Now()
+		return currentTime.Format(time.RFC3339)
+	case "email":
+		return "yyy@xx.com"
+	}
+	return ""
 }
 
 func LogErrorIfAny(description string, err error, data ...interface{}) {
